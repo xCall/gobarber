d5709ce0376f9687eb6a4dac65bbeaaf4e1c59f2
xCall <carlosfaita99@gmail.com>
2022-01-20 16:55:49 -0300

    Iniciado upload de avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import { verify } from 'jsonwebtoken';
+import { verify } from "jsonwebtoken";
 
-import authConfig from '../config/auth';
+import authConfig from "../config/auth";
 
 interface ITokenPayload {
   iat: number;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserService } from '../services/CreateUserService';
 
 const port = 3333;
 
 const usersRouter = Router();
+const upload = multer(uploadConfig.upload('./tmp'));
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -18,13 +22,29 @@ usersRouter.post('/', async (request, response) => {
 
     delete user.password;
 
-    console.log('\x1b[34m', `✔ POST - (Users) ➡️  http://localhost:${port}/users`);
+    console.log(
+      '\x1b[34m',
+      `✔ POST - (Users) ➡️  http://localhost:${port}/users`,
+    );
 
     return response.json(user);
   } catch (err) {
-    console.log('\x1b[31m', `✗ POST - (Users) ➡️  http://localhost:${port}/users`);
+    console.log(
+      '\x1b[31m',
+      `✗ POST - (Users) ➡️  http://localhost:${port}/users`,
+    );
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const { avatar } = request.file;
+    return response.json({ ok: true });
+  },
+);
 
 export { usersRouter };
